b51f441e1a75dd2f4e64c33391f8875051a095c1
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-03-29 17:54:52 -0300

    chore: pathing with babel-plugin-module-resolver

--- teams/babel.config.js
+++ teams/babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./assets",
+            "@components": "./src/components",
+            "@hooks": "./src/hooks",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@types": "./src/types",
+            "@storage": "./src/storage",
+            "@routes": "./src/routes",
+            "@theme": "./src/theme",
+          },
+        },
+      ],
+    ],
   };
 };
