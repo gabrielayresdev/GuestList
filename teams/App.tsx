5397d4157ab91b237a14c49d35854f3799d005c6
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-01 16:46:55 -0300

    feat: adding google fonts

--- teams/App.tsx
+++ teams/App.tsx
@@ -1,12 +1,19 @@
 import Groups from "@screens/Groups";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
