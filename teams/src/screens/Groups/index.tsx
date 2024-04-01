de92eb905c9f20f8ef4dc8fd8c4c80a06011b09c
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-01 16:37:17 -0300

    feat: project theme created

---
+++ teams/src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Text } from "./styles";
+
+const Groups = () => {
+  return (
+    <Container>
+      <Text>Groups</Text>
+    </Container>
+  );
+};
+
+export default Groups;
