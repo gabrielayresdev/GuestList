99c581f6f4d2f9b0789d029c61f6b9e56f3f9ba9
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-04 19:25:14 -0300

    feat: header

--- teams/App.tsx
+++ teams/App.tsx
@@ -13,7 +13,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import Loading from "src/Loading";
+import Loading from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container, Text } from "./styles";
+import Header from "@components/Header";
 
 const Groups = () => {
   return (
     <Container>
-      <Text>Groups</Text>
+      <Header />
     </Container>
   );
 };
