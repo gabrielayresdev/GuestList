91beeb4f0c44123d07f0de8794909b2c401422b6
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-05 00:35:01 -0300

    feat: title and subtitle component

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container, Text } from "./styles";
 import Header from "@components/Header";
+import Highlight from "@components/Highlight";
 
 const Groups = () => {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 };
