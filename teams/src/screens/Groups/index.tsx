d746ff89ac30f0e94602cb056a12f349ae661c7c
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-15 20:11:35 -0300

    feat: GroupCard list

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import { Container, Text } from "./styles";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
+import { FlatList } from "react-native";
+import { GroupCard } from "@components/GroupCard/indext";
 
 const Groups = () => {
+  const [groups, setGroups] = React.useState<string[]>(["Depois da 00"]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 };
