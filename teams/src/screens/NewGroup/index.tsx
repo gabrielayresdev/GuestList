fb74fc228e63f910444528a122acef2391e23827
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-16 15:24:17 -0300

    feat: NewGroup screen

---
+++ teams/src/screens/NewGroup/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Content, Icon } from "./styles";
+import Header from "@components/Header";
+import Highlight from "@components/Highlight";
+import Button from "@components/Button";
+
+const NewGroup = () => {
+  return (
+    <Container>
+      <Header showBackButton />
+      <Content>
+        <Icon />
+        <Highlight
+          title="Nova turma"
+          subtitle="crie a turma para adicionar as pessoas"
+        />
+
+        <Button title="Criar" />
+      </Content>
+    </Container>
+  );
+};
+
+export default NewGroup;
