776dc11091e4729f86c5fef464f711d5dbd2c51c
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-26 14:01:45 -0300

    feat: dealing with exceptions

--- teams/src/screens/NewGroup/index.tsx
+++ teams/src/screens/NewGroup/index.tsx
@@ -6,6 +6,8 @@ import Button from "@components/Button";
 import Input from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 const NewGroup = () => {
   const [group, setGroup] = React.useState("");
@@ -13,8 +15,17 @@ const NewGroup = () => {
   const navigation = useNavigation();
 
   async function handleNew() {
-    await groupCreate(group);
-    navigation.navigate("players", { group });
+    try {
+      if (group.trim().length === 0) return;
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo grupo", "Não foi possível criar um novo grupo.");
+      }
+    }
   }
 
   return (
